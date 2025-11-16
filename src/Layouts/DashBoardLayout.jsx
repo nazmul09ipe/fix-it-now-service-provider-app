f0a86c15623535fd78df8205582f4bc9c14c3af4
nazmul haque <nazmul09ipe@gmail.com>
2025-11-16 17:10:13 +0600

    changed

--- src/Layouts/DashBoardLayout.jsx
+++ src/Layouts/DashBoardLayout.jsx
@@ -6,16 +6,14 @@ import { Outlet } from 'react-router';
 const DashBoardLayout = () => {
     return (
          <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-950 transition-colors duration-300">
-      {/* Navbar */}
+    
       <Navbar />
 
-      {/* Main content area */}
       <main className="grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-20">
-        {/* Outlet renders the page-specific content */}
+        
         <Outlet />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
     );
